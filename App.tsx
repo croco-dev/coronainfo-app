fcdf545dc45fb12fd65491fda5bb2f44ab4c94f8
kang-heewon <itphantom@icloud.com>
2020-03-01 23:42:06 +0900

    fix:🔥android permission error

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Platform } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 export default function App() {
@@ -7,7 +6,7 @@ export default function App() {
     <WebView
       source={{ uri: 'https://coronas.info/' }}
       scalesPageToFit={false}
-      style={{ marginTop: Platform.OS === 'ios' ? 30 : 0 }}
+      style={{ marginTop: 30 }}
     />
   );
 }
