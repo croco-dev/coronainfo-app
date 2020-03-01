c695e59e005bbb38a4deeacfb53c72beab515a9e
kang-heewon <itphantom@icloud.com>
2020-03-02 00:27:26 +0900

    style:👁safe area

--- App.tsx
+++ App.tsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import { WebView } from 'react-native-webview';
+import { SafeAreaView, Platform, StatusBar, Linking } from 'react-native';
 
 export default function App() {
+  const uri = 'https://coronas.info/';
   return (
-    <WebView
-      source={{ uri: 'https://coronas.info/' }}
-      scalesPageToFit={false}
-      style={{ marginTop: 30 }}
-    />
+    <SafeAreaView
+      style={{
+        flex: 1,
+        paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+      }}
+    >
+      <StatusBar translucent />
+      <WebView
+        ref={ref => {
+          this.webview = ref;
+        }}
+        source={{ uri }}
+        scalesPageToFit={false}
+        onNavigationStateChange={event => {
+          if (!event.url.includes(uri)) {
+            this.webview.stopLoading();
+            Linking.openURL(event.url);
+          }
+        }}
+      />
+    </SafeAreaView>
   );
 }
